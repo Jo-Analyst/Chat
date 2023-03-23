a7169724df2b6182e2127db5af7bf9abb2bbdbb0
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-03-23 10:53:14 -0300

    Auth Page #04

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -13,7 +13,8 @@ class _AuthFormState extends State<AuthForm> {
   final _formData = AuthFormData();
 
   void _submit() {
-    _formKey.currentState?.validate();
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
   }
 
   @override
@@ -32,11 +33,27 @@ class _AuthFormState extends State<AuthForm> {
                   initialValue: _formData.name,
                   onChanged: (name) => _formData.name = name,
                   decoration: const InputDecoration(labelText: "Nome"),
+                  validator: (value) {
+                    final name = value ?? "";
+                    if (name.trim().length < 3) {
+                      return "Nome deve ter no mínimo 3 caracteres.";
+                    }
+
+                    return null;
+                  },
                 ),
               TextFormField(
                 key: const ValueKey("email"),
                 onChanged: (email) => _formData.email = email,
                 decoration: const InputDecoration(labelText: "E-mail"),
+                validator: (value) {
+                  final email = value ?? "";
+                  if (!email.contains("@")) {
+                    return "Email informado não é válido.";
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
                 key: const ValueKey("password"),
@@ -44,6 +61,14 @@ class _AuthFormState extends State<AuthForm> {
                 onChanged: (password) => _formData.password = password,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: "Senha"),
+                validator: (value) {
+                  final password = value ?? "";
+                  if (password.trim().length < 6) {
+                    return "Senha deve ter no mínimo 6 caracteres.";
+                  }
+
+                  return null;
+                },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
