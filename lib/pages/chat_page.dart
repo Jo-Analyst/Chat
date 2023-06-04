bd67a1094903a893ff2410a416ed37c30915b890
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-06-04 10:01:30 -0300

    Icone notifications

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notifications/chat_notification_service.dart';
+import 'package:chat/pages/notifcation_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -11,30 +14,58 @@ class ChatPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: "logout",
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(width: 10),
+                      Text("Sair")
+                    ],
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == "logout") AuthService().logout();
+              },
             ),
-            items: [
-              DropdownMenuItem(
-                value: "logout",
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.exit_to_app,
-                      color: Colors.black87,
-                    ),
-                    SizedBox(width: 10),
-                    Text("Sair")
-                  ],
+          ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) {
+                      return const NotificationPage();
+                    }),
+                  );
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  backgroundColor: Colors.red.shade800,
+                  maxRadius: 10,
+                  child: Text(
+                    "${Provider.of<ChatNotificationService>(context).itemsCount}",
+                    style: const TextStyle(fontSize: 12),
+                  ),
                 ),
               )
             ],
-            onChanged: (value) {
-              if (value == "logout") AuthService().logout();
-            },
-          )
+          ),
         ],
         centerTitle: true,
         title: const Text("JODEV Chat"),
