0c63b33a1f7fe1c2d63778850fa8b1e9d9a19e5f
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-06-10 08:54:11 -0300

    Push Notification #01

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat/core/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -20,5 +21,28 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  // Push Notification
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<bool> get _isAuthorirized async {
+    final messaging = FirebaseMessaging.instance;
+    final setting = await messaging.requestPermission();
+    return setting.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorirized) {
+      FirebaseMessaging.onMessage.listen((msg) {
+        if (msg.notification == null) return;
+        add(ChatNotification(
+          title: msg.notification!.title ?? "Não informado!",
+          body: msg.notification!.body ?? "Não informado!",
+        ));
+      });
+    }
   }
 }
